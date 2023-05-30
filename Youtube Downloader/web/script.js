d76ac58444b8ab85e0b287d8c3a30d947aafd45f
JavokhirUzCoder <131436276+JavokhirUzCoder@users.noreply.github.com>
2023-05-30 17:43:58 +0500

    Add files via upload

--- Youtube Downloader/web/script.js
+++ Youtube Downloader/web/script.js
@@ -6,11 +6,16 @@ let btn = document.getElementById('runner');
 let url = document.getElementById('link');
 let result = document.getElementById('result');
 
-
+url.addEventListener('input', function(e) {
+    if (url.value == ''){
+        result.textContent = "Enter Youtube link.";
+    }
+});
 
 btn.addEventListener('click', function(e) {
     link = url.value;
     console.log(link);
+    result.textContent = "Downloading ...";
     eel.MainFunc(link)(e =>{
         result.textContent = e;
         if (e == "Something is wrong"){
